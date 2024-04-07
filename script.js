d466b15b73f0958db85fdb9ffc80797218c86ec3
L2W <145001628+MYL2W@users.noreply.github.com>
2024-04-07 19:24:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Получаем кнопку "Socials" и контейнер для кнопок с иконками
+var socialsBtn = document.querySelector('.socials-btn');
+var socialBtnsContainer = document.querySelector('.social-btns-container');
 
+// Обработчик события нажатия на кнопку "Socials"
+socialsBtn.addEventListener('click', function() {
+    // Скрываем кнопку "Socials"
+    socialsBtn.style.display = 'none';
+    // Показываем контейнер с кнопками с иконками
+    socialBtnsContainer.style.display = 'block';
+});
